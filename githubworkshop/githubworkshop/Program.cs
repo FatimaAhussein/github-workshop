e694e3b3a316671d9010b21c134df1eb18903d36
HaniehJ97 <haniehjafari642@gmail.com>
2025-02-14 13:01:58 +0100

    hannieh

--- githubworkshop/githubworkshop/Program.cs
+++ githubworkshop/githubworkshop/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("THis is Hanna writing");
         }
     }
 }
